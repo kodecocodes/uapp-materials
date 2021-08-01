0748cd8f60f207295f9fb1bf61d7e202d075b859
Eric Van de Kerckhove <BlackDragonBE@users.noreply.github.com>
2021-08-01 09:54:04 +0200

    Chapter 4 starter

---
+++ 04-creating-and-using-scripts-with-c-sharp/projects/starter/Chapter 4 Starter/Assets/RW/Scripts/Ammunition/Ammunition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ammunition : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * movementSpeed * Time.deltaTime, Space.World);
+
+        if (transform.position.x > 20f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 04-creating-and-using-scripts-with-c-sharp/projects/starter/Chapter 4 Starter/Assets/RW/Scripts/UI/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text ammunitionAmountText;
+    public Slider ammuntionReadySlider;
+
+    public void InitialSetup(float maximumTimerValue)
+    {
+        ammunitionAmountText.text = "0";
+        ammuntionReadySlider.maxValue = maximumTimerValue;
+    }
+
+    public void UpdateAmmunitionText(int value)
+    {
+        ammunitionAmountText.text = value.ToString();
+    }
+
+    public void UpdateAmmunitionTimerValue(float value)
+    {
+        ammuntionReadySlider.value = value;
+    }
+}
