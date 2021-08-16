2216c2ffc7488e2784b25fbcf892b14e5a12f010
Matt Larson <larsonmattr@gmail.com>
2021-08-15 19:01:21 -0500

    Renamed scripts as controllers (Player/Enemy), added Copyrights, and using mouse screen position to aim the fork cannon.

--- 09-basic-ai-and-navigation/projects/final/Chapter 9 Final/Assets/RW/Scripts/GameState.cs
+++ 09-basic-ai-and-navigation/projects/final/Chapter 9 Final/Assets/RW/Scripts/GameState.cs
@@ -1,3 +1,35 @@
+/// Copyright (c) 2021 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -34,7 +66,7 @@ public class GameState : MonoBehaviour
 
     private void Reset()
     {
-        player.GetComponent<MoveToPointClick>().Reset();
+        player.GetComponent<PlayerController>().Reset();
         state = States.Countdown;
         timeRemaining = 3;
         UpdateGUI();
@@ -45,7 +77,7 @@ public class GameState : MonoBehaviour
     {
         timeRemaining -= Time.deltaTime;
 
-        if (player.GetComponent<MoveToPointClick>().GetHealth() < 0)
+        if (player.GetComponent<PlayerController>().GetHealth() < 0)
         {
             state = States.Lose;
             player.GetComponent<NavMeshAgent>().isStopped = true;
@@ -109,6 +141,6 @@ public class GameState : MonoBehaviour
                 break;
         }
 
-        HealthBar.sizeDelta = new Vector2(735 * player.GetComponent<MoveToPointClick>().GetPercentHealth(), 65);
+        HealthBar.sizeDelta = new Vector2(735 * player.GetComponent<PlayerController>().GetPercentHealth(), 65);
     }
 }
